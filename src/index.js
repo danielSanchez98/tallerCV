61cef1c4970810fb255fe8c7762e7c23bdc36fd6
danielSanchez98 <ds980220@gmail.com>
2020-05-15 11:11:09 -0500

    Sevidor y configuracion

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('Working');
+const express = require('express'),
+      path= require('path'),
+      app= express(),
+      cookieParser= require('cookie-parser'),
+      cookieSession= require('cookie-session');
+
+
+// Settings
+app.set('port', 3000);
+app.set('views', path.resolve(__dirname,'views'));
+app.set('view engine','ejs');
+
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// Rutas
+
+app.get('/',(req,res)=>{
+
+    res.render('index')
+
+});
+
+
+app.listen(app.get('port'),()=>{
+    console.log(`Server on port ${app.get('port')}`);
+
+});
